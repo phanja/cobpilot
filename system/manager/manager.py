0334bf7757351272d1b3db68aabb2739d36e7265
James <91348155+FrogAi@users.noreply.github.com>
2025-12-01 12:00:00 -0700

    FrogPilot structs

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -142,6 +142,8 @@ def manager_thread() -> None:
   ignition_prev = False
 
   # FrogPilot variables
+  sm = sm.extend(['frogpilotPlan'])
+
   params_memory = Params(memory=True, return_defaults=True)
 
   while True:
