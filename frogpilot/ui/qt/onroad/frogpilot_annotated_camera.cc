6ade849391a58ec70384d0f723b4ba255a5b28f8
James <91348155+FrogAi@users.noreply.github.com>
2025-12-01 12:00:00 -0700

    Pause Acceleration/Braking

--- frogpilot/ui/qt/onroad/frogpilot_annotated_camera.cc
+++ frogpilot/ui/qt/onroad/frogpilot_annotated_camera.cc
@@ -147,6 +147,7 @@ void FrogPilotAnnotatedCameraWidget::updateState(const UIState &s, const FrogPil
   forceCoast = frogpilotCarState.getForceCoast();
   laneWidthLeft = frogpilotPlan.getLaneWidthLeft();
   laneWidthRight = frogpilotPlan.getLaneWidthRight();
+  longitudinalPaused = frogpilotCarState.getPauseLongitudinal();
   redLight = frogpilotPlan.getRedLight();
   roadCurvature = frogpilotPlan.getRoadCurvature();
   roadName = QString::fromStdString(params_memory.get("RoadName"));
@@ -220,7 +221,7 @@ void FrogPilotAnnotatedCameraWidget::paintFrogPilotWidgets(QPainter &p, UIState 
     }
   }
 
-  if (!hideBottomIcons && (forceCoast)) {
+  if (!hideBottomIcons && (forceCoast || longitudinalPaused)) {
     paintLongitudinalPaused(p);
   }
 
--- frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
+++ frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
@@ -83,6 +83,7 @@ private:
   bool cscTraining;
   bool experimentalMode;
   bool forceCoast;
+  bool longitudinalPaused;
   bool redLight;
 
   int animationFrameIndex;
