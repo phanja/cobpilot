3da8e01db907006bf9b600994b76f0ac1e97ae38
James <91348155+FrogAi@users.noreply.github.com>
2025-12-01 12:00:00 -0700

    Retain network tethering

--- frogpilot/ui/frogpilot_ui.cc
+++ frogpilot/ui/frogpilot_ui.cc
@@ -41,6 +41,10 @@ FrogPilotUIState::FrogPilotUIState(QObject *parent) : QObject(parent) {
   wifi = new WifiManager(this);
 
   frogpilot_scene.frogpilot_toggles = QJsonDocument::fromJson(QByteArray((*sm)["frogpilotPlan"].getFrogpilotPlan().getFrogpilotToggles().cStr())).object();
+
+  if (frogpilot_scene.frogpilot_toggles.value("tethering_config").toInt() == 1) {
+    wifi->setTetheringEnabled(true);
+  }
 }
 
 FrogPilotUIState *frogpilotUIState() {
