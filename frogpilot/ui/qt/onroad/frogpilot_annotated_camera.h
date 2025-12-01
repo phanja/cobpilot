7cf018f12d0958da3fc94f156417a0d80afda2b0
James <91348155+FrogAi@users.noreply.github.com>
2025-12-01 12:00:00 -0700

    Lead Info

--- frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
+++ frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
@@ -19,6 +19,7 @@ public:
   void paintAdjacentPaths(QPainter &p);
   void paintBlindSpotPath(QPainter &p);
   void paintFrogPilotWidgets(QPainter &p, UIState &s);
+  void paintLeadMetrics(QPainter &p, bool adjacent, QPointF *chevron, const cereal::RadarState::LeadData::Reader &lead_data);
   void updateState(const UIState &s, const FrogPilotUIState &fs);
 
   bool hideBottomIcons;
@@ -45,6 +46,8 @@ public:
 
   QPolygonF track_adjacent_vertices[2];
 
+  QRect adjacentLeadTextRect;
+  QRect leadTextRect;
   QRect setSpeedRect;
 
   QSize defaultSize;
@@ -80,6 +83,7 @@ private:
   bool redLight;
 
   int animationFrameIndex;
+  int desiredFollowDistance;
   int frogHopCount;
   int signalAnimationLength;
   int signalHeight;
